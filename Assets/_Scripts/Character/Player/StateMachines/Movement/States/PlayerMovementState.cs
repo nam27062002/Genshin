b89dcbc9ed77b8da60ef32e56a495dd8d5f398ea
nam <nam27062002@gmail.com>
2023-06-26 00:10:55 +0700

    update lần cuối để đi ngủ mệt quá rồi

--- Assets/_Scripts/Character/Player/StateMachines/Movement/States/PlayerMovementState.cs
+++ Assets/_Scripts/Character/Player/StateMachines/Movement/States/PlayerMovementState.cs
@@ -4,26 +4,27 @@ public class PlayerMovementState : IState
 {
     public virtual void Enter()
     {
-        Debug.Log("State : " + GetType().Name);
+        // Ghi lại trạng thái hiện tại
+        Debug.Log("State: " + GetType().Name);
     }
 
     public virtual void Exit()
     {
-        
+        // Xử lý khi rời khỏi trạng thái
     }
 
     public virtual void HandleInput()
     {
-        
+        // Xử lý các tương tác đầu vào
     }
 
     public virtual void PhysicsUpdate()
     {
-        
+        // Xử lý logic liên quan đến vật lý (sử dụng FixedUpdate)
     }
 
     public virtual void Update()
     {
-        
+        // Xử lý logic cập nhật (sử dụng Update)
     }
 }
--- Assets/_Scripts/Character/Player/Utilities/Input/PlayerInput.cs
+++ Assets/_Scripts/Character/Player/Utilities/Input/PlayerInput.cs
@@ -7,17 +7,21 @@ public class PlayerInput : MonoBehaviour
 
     private void Awake()
     {
+        // Khởi tạo đối tượng PlayerInputActions để quản lý các hành động người chơi
         InputActions = new PlayerInputActions();
+        // Lấy ra hành động người chơi từ PlayerInputActions để sử dụng
         PlayerActions = InputActions.Player;
     }
 
     private void OnEnable()
     {
+        // Bật kích hoạt các hành động người chơi
         InputActions.Enable();
     }
 
     private void OnDisable()
     {
+        // Tắt kích hoạt các hành động người chơi
         InputActions.Disable();
     }
 }
